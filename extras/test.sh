af5f89a707e099a039e1e076ec45a5efd710d579
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2015-10-10 11:50:27 +0200

    Functional tests for the writer.

--- extras/test.sh
+++ extras/test.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
+# Test whether the YAML produced by the JavaScript version equals that of the
+# Python version.
 test_cli() {
   seed=${RANDOM}
   py="/tmp/${seed}_py.yml"
   js="/tmp/${seed}_js.yml"
 
-  python -m python.cli $1 $2 $3 $py
+  python -m python.cli read $1 $2 $3 $py
   nodejs javascript/cli.js $1 $2 $3 > $js
 
-  echo $1
+  echo "  $1"
   python -m extras.compare_yaml $py $js
 
   rm $py $js
 }
 
+# Test whether the generated YAML is invariant under writing to binary and
+# back.
+test_writer() {
+  seed=${RANDOM}
+  yml_1="/tmp/${seed}_1.yml"
+  yml_2="/tmp/${seed}_2.yml"
+  bin="/tmp/${seed}.bin"
+
+  python -m python.cli read $1 $2 $3 $yml_1
+  python -m python.cli write $yml_1 $2 $3 $bin
+  python -m python.cli read $bin $2 $3 $yml_2
+
+  echo "  $1"
+  python -m extras.compare_yaml $yml_1 $yml_2
+
+  cat $yml_2
+  rm $yml_1 $yml_2 $bin
+}
+
+echo Python vs. JavaScript tests:
 test_cli examples/balance/balance.dat examples/balance/structure.yml \
   examples/balance/types.yml
 test_cli examples/conditional/a.dat examples/conditional/structure.yml \
@@ -29,3 +51,19 @@ test_cli examples/lists/for.dat examples/lists/structure_for.yml \
 test_cli examples/lists/while.dat examples/lists/structure_while.yml \
   examples/lists/types.yml
 
+echo
+echo Writer tests:
+test_writer examples/balance/balance.dat examples/balance/structure.yml \
+  examples/balance/types.yml
+test_writer examples/conditional/a.dat examples/conditional/structure.yml \
+  examples/conditional/types.yml
+test_writer examples/conditional/b.dat examples/conditional/structure.yml \
+  examples/conditional/types.yml
+test_writer examples/csv/test.csv examples/csv/structure.yml \
+  examples/csv/types.yml
+test_writer examples/lists/do_while.dat examples/lists/structure_do_while.yml \
+  examples/lists/types.yml
+test_writer examples/lists/for.dat examples/lists/structure_for.yml \
+  examples/lists/types.yml
+test_writer examples/lists/while.dat examples/lists/structure_while.yml \
+  examples/lists/types.yml
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -30,70 +30,53 @@ class TestReader(object):
             open('examples/conditional/structure.yml'),
             open('examples/conditional/types.yml')).parsed
 
-
     def test_balance_1(self):
         assert self.balance['name'] == 'John Doe'
-
 
     def test_balance_2(self):
         assert self.balance['year_of_birth'] == 1999
 
-
     def test_balance_3(self):
         assert self.balance['balance'] == 3210
-
 
     def test_for_1(self):
         assert self.for_['size_of_list'] == 5
 
-
     def test_for_2(self):
         assert len(self.for_['lines']) == 5
-
 
     def test_for_3(self):
         assert self.for_['lines'][0]['content'] == 'line1'
 
-
     def test_for_4(self):
         assert self.for_['lines'][4]['content'] == 'last'
-
 
     def test_do_while_1(self):
         assert len(self.do_while['lines']) == 5
 
-
     def test_do_while_2(self):
         assert self.do_while['lines'][0]['id'] == 0x01
-
 
     def test_do_while_3(self):
         assert self.do_while['lines'][4]['id'] == 0x02
 
-
     def test_while_1(self):
         assert len(self.while_['lines']) == 5
-
 
     def test_while_2(self):
         assert self.while_['lines'][4]['id'] == 0x01
 
-
     def test_while_3(self):
         assert self.while_['lines_term'] == 0x02
-
 
     def test_if_1(self):
         assert 'related_to_b' not in self.if_a
 
-
     def test_if_2(self):
         assert self.if_a['related_to_a'] == 'not skipped'
 
-
     def test_if_3(self):
         assert 'related_to_a' not in self.if_b
-
 
     def test_if_4(self):
         assert self.if_b['related_to_b'] == 'not skipped'
