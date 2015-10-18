3ab7519b69cad026021b94be4ffc4a471b2a4c55
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2015-10-18 15:52:06 +0200

    Version bump.

--- python/__init__.py
+++ python/__init__.py
@@ -10,8 +10,7 @@ from .bin_parser import BinReader, BinWriter
 from .functions import BinReadFunctions, BinWriteFunctions
 
 
-__version_info__ = ('0', '0', '8')
-__date__ = '4 Oct 2015'
+__version_info__ = ('0', '0', '9')
 
 __version__ = '.'.join(__version_info__)
 __author__ = 'Jeroen F.J. Laros'
