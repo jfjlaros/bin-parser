46e631c1212460097293d665ddb419c14341da0f
Jamie Ross <jamie.ross@electricityexchange.ie>
2018-02-01 10:42:18 +0000

    modified the example to read and write floating point values

--- examples/floatingpoint/parser.js
+++ examples/floatingpoint/parser.js
@@ -6,14 +6,30 @@ var fs = require('fs'),
     yaml = require('js-yaml');
 
 var BinParser = require('../../javascript/index');
-
+console.log("== starting read test ==");
 var parser = new BinParser.BinReader(
-  fs.readFileSync('balance.dat'),
-  yaml.load(fs.readFileSync('structure.yml')),
+  fs.readFileSync('float_input.dat'),
+  yaml.load(fs.readFileSync('structure_float.yml')),
   yaml.load(fs.readFileSync('types.yml')),
   {})
 
-console.log(parser.parsed.name);
-console.log(parser.parsed.year_of_birth);
-console.log(parser.parsed.balance);
-console.log(parser.parsed.number);
+console.log(parser.parsed.voltage);
+console.log(parser.parsed.frequency);
+console.log(parser.parsed.current);
+
+ console.log("== starting write test ==")
+ function main() {
+   var parser = new BinParser.BinWriter(
+     yaml.load(fs.readFileSync('./input.yml')),
+     yaml.load(fs.readFileSync('./structure_float.yml')),
+     yaml.load(fs.readFileSync('./types.yml')),
+     {});
+     console.log("binary output",(parser.data));
+     process.stdout.write((parser.data));
+ }
+
+ // Wait for the stdout buffer to drain, see
+ // https://github.com/eslint/eslint/issues/317
+ process.on('exit', function() {
+   process.reallyExit(main());
+ });
