0b43a115c5590ea39e3637eea012a87b81cc0039
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-05-17 22:22:02 +0200

    Rewritten functions module tests.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -7,67 +7,87 @@ from bin_parser import functions
 class TestParser(object):
     """Test the bin_parser.functions module."""
     def setup(self):
-        self.brf = functions.BinReadFunctions()
-        self.bwf = functions.BinWriteFunctions()
-        self.string = '012'
-        self.byte = '0'
-        self.annotation = 'xxxx'
-        self.float = '\x42\xf6\xcc\xcd'
-        self.date_annotation = {3289392: self.annotation}
-        self.map_annotation = {48: self.annotation}
-        self.flags_annotation = {0x10: self.annotation, 0x01: 'unused'}
+        self._brf = functions.BinReadFunctions()
+        self._bwf = functions.BinWriteFunctions()
 
-    def test_raw(self):
-        assert self.string == self.bwf.raw(self.brf.raw(self.string))
+    def _idem(self, func, data, **kwargs):
+        """General idempotency test.
+
+        This test decodes `data`, encodes the result and checks whether this
+        equals `data`.
+
+        :arg function func: Function to be tested.
+        :arg any data: Data for `func`.
+        :arg dict **kwargs: Arguments for `func`.
+        """
+        assert getattr(self._bwf, func)(
+            getattr(self._brf, func)(data, **kwargs), **kwargs) == data
+
+    def test_raw_single(self):
+        assert self._brf.raw('\x03') == '03'
+
+    def test_raw_multi(self):
+        assert self._brf.raw('\x00\x01\x02') == '00 01 02'
+
+    def test_raw_idem(self):
+        self._idem('raw', '\x00\x01\x02')
 
     def test_bit(self):
-        assert self.byte == self.bwf.bit(self.brf.bit(self.byte))
+        assert self._brf.bit('\x03') == '00000011'
 
-    def test_int_1(self):
-        assert self.string == self.bwf.int(self.brf.int(self.string))
+    def test_bit_idem(self):
+        self._idem('bit', '\x03')
 
-    def test_int_2(self):
-        assert self.bwf.int(0) == chr(0x00)
+    def test_le_s_short(self):
+        assert self._brf.struct('\x01\x00', fmt='<h') == 1
 
-    def test_float(self):
-        assert self.float == self.bwf.float(self.brf.float(self.float))
+    def test_be_s_short(self):
+        assert self._brf.struct('\x00\x01', fmt='>h') == 1
 
-    def test_colour(self):
-        assert self.string == self.bwf.colour(self.brf.colour(self.string))
+    def test_le_s_short_idem(self):
+        self._idem('struct', '\x01\x02', fmt='<h')
 
-    def test_date_1(self):
-        assert self.string == self.bwf.date(
-            self.brf.date(self.string, {}), {})
+    def test_be_s_float_idem(self):
+        self._idem('struct', '\x01\x02\x03\x04', fmt='>f')
 
-    def test_date_2(self):
-        assert self.brf.date(
-            self.string, self.date_annotation) == self.annotation
+    def test_labels(self):
+        assert self._brf.struct(
+            '\x01\x02', fmt='BB', labels=['a', 'b']) == {'a': 1, 'b': 2}
 
-    def test_date_3(self):
-        assert self.string == self.bwf.date(
-            self.brf.date(self.string, self.date_annotation),
-            self.date_annotation)
+    def test_labels_idem(self):
+        self._idem('struct', '\x01\x02', fmt='BB', labels=['a', 'b'])
 
-    def test_map_1(self):
-        assert self.byte == self.bwf.map(self.brf.map(self.byte, {}), {})
+    def test_annotation(self):
+        assert self._brf.struct(
+            '\x01\x02', fmt='BB', annotation={1: 'x'}) == ['x', 2]
 
-    def test_map_2(self):
-        assert self.brf.map(
-            self.byte, self.map_annotation) == self.annotation
+    def test_annotation_idem(self):
+        self._idem('struct', '\x01\x02', fmt='BB', annotation={1: 'a'})
 
-    def test_map_3(self):
-        assert self.byte == self.bwf.map(
-            self.brf.map(self.byte, self.map_annotation), self.map_annotation)
+    def test_labels_annotation(self):
+        assert self._brf.struct(
+            '\x01\x02', fmt='BB', labels=['a', 'b'],
+            annotation={1: 'x'}) == {'a': 'x', 'b': 2}
 
-    def test_flags_1(self):
-        assert self.byte == self.bwf.flags(self.brf.flags(self.byte, {}), {})
+    def test_labels_annotation_idem(self):
+        self._idem(
+            'struct', '\x01\x02', fmt='BB', labels=['a', 'b'],
+            annotation={1: 'x'})
 
-    def test_flags_2(self):
-        assert (self.brf.flags(
-            self.byte, self.flags_annotation) ==
-            {'flag_20': True, 'unused': False, 'xxxx': True})
+    def test_flags(self):
+        assert self._brf.flags('\x03', {}) == {'flag_01': True, 'flag_02': True}
 
-    def test_flags_3(self):
-        assert self.bwf.flags(self.brf.flags(
-            self.byte, self.flags_annotation),
-            self.flags_annotation) == self.byte
+    def test_flags_false(self):
+        assert self._bwf.flags({'flag_01': True, 'flag_02': False}, {}) == '\x01'
+
+    def test_flags_idem(self):
+        self._idem('flags', '\x03', annotation={})
+
+    def test_flags_annotation(self):
+        assert self._brf.flags('\x03', {2: 'a'}) == {'flag_01': True, 'a': True}
+
+    def test_flags_annotation_false(self):
+        assert self._brf.flags('\x01', {2: 'a'}) == {'flag_01': True, 'a': False}
+
+    def test_flags_annotation_idem(self):
+        self._idem('flags', '\x03', annotation={2: 'a'})
